974bc7365aca03396b29b6536839ebbeabb0c085
0xIslamTaha <islamtaha2012@gmail.com>
2019-01-27 17:19:05 +0200

    variable, methods, object and class

---
+++ src/JourneyToJava001.java
@@ -0,0 +1,34 @@
+public class JourneyToJava001 {
+
+    double addition(double x, double y){
+        return x+y;
+    }
+
+    double subtraction(double x, double y){
+        return x-y;
+    }
+
+    double multi(double x, double y){
+        return x*y;
+    }
+
+    double divde(double x, double y){
+        return x/y;
+    }
+
+    public static void main(String[] args) {
+        int int_v = 10; // Integer numbers
+        double dou_v = 140.13; // floating numbers
+        char char_v = 'a'; // one character
+        String str_v = "JourneyToJAva By Islam Taha"; // string
+
+        System.out.println(int_v);
+        System.out.println(dou_v);
+        System.out.println(char_v);
+        System.out.println(str_v);
+
+        JourneyToJava001 journey_obj = new JourneyToJava001();
+        System.out.println(journey_obj.addition(10.1, 10.2));
+
+    }
+}
