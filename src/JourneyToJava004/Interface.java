9a99c375002751d7b1aabe0639a3ac03d7d223de
0xIslamTaha <islamtaha2012@gmail.com>
2019-02-11 17:36:07 +0200

    interface

--- src/JourneyToJava004/Interface.java
+++ src/JourneyToJava004/Interface.java
@@ -1,4 +1,22 @@
 package JourneyToJava004;
 
-public class Interface {
+
+
+interface Interface_ex {
+    public void implementME();
+}
+
+
+class implementation implements Interface_ex{
+    @Override
+    public void implementME() {
+        System.out.println("Here is my body!");
+    }
+}
+
+public class Interface{
+    public static void main(String args[]) {
+        implementation obj = new implementation();
+        obj.implementME();
+    }
 }
